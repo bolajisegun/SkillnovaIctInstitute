5208afd7e3b3e7077fca1f4685dbc9c3ae348403
bolajisegun <bolajisegun2015@gmail.com>
2025-07-27 01:34:54 +0100

    updated some changes on the card

--- src/app/Component/homepageCard.tsx
+++ src/app/Component/homepageCard.tsx
@@ -7,21 +7,21 @@ import Link from "next/link";
 const HomepageCard = () => {
   return (
     <div>
-      <div className="courseImg w-[350px] bg-white rounded-md shadow-md p-4 flex flex-col items-center">
+      <div className="courseImg w-[250px] md:w-[350px] bg-white rounded-md shadow-md p-4 flex flex-col items-center">
         <Image src={CourseImage} alt="Course Image" />
         <div className="courseInfo">
-          <h5 className="courseSubtitle text-[#4A90E2] my-4 text-[12px] font-semibold">
+          <h5 className="courseSubtitle text-[#4A90E2] my-4 text-[10px] md:text-[12px] font-semibold">
             Programming
           </h5>
-          <h3 className="courseTitle font-bold mb-1 text-[15px]">
+          <h3 className="courseTitle font-bold mb-1 text-[13px] md:text-[15px]">
             React & Next.js Full Course
           </h3>
-          <p className="courseDescription text-[13px]">
+          <p className="courseDescription text-[12px] md:text-[13px]">
             Build modern, high-performance web applications with React and
             Next.js, including server-side rendering and API integrations.
           </p>
           <div className="courseActions flex items-center justify-between mt-4 w-full">
-            <div className="rating flex items-center gap-1 text-md text-yellow-500">
+            <div className="rating flex items-center gap-1 text-sm md:text-md text-yellow-500">
               <CiStar />
               <CiStar />
               <CiStar />
@@ -31,7 +31,7 @@ const HomepageCard = () => {
                 5.0<span className="text-gray-500 font-normal">(1200)</span>
               </span>
             </div>
-            <div className="price font-semibold text-[#4A90E2]">
+            <div className="price font-semibold text-[#4A90E2] text-[13px] md:text-[15spx]">
               <h5>#1,500</h5>
             </div>
           </div>
