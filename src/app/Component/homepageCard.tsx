9f3aca44d11a0b4dba0e4aaaabefd7563a7517cd
bolajisegun <bolajisegun2015@gmail.com>
2025-07-27 01:19:39 +0100

    started the course card on homepage

---
+++ src/app/Component/homepageCard.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import React from "react";
+import CourseImage from "../images/react-next-js.png";
+import { CiStar } from "react-icons/ci";
+import Link from "next/link";
+
+const HomepageCard = () => {
+  return (
+    <div>
+      <div className="courseImg w-[350px] bg-white rounded-md shadow-md p-4 flex flex-col items-center">
+        <Image src={CourseImage} alt="Course Image" />
+        <div className="courseInfo">
+          <h5 className="courseSubtitle text-[#4A90E2] my-4 text-[12px] font-semibold">
+            Programming
+          </h5>
+          <h3 className="courseTitle font-bold mb-1 text-[15px]">
+            React & Next.js Full Course
+          </h3>
+          <p className="courseDescription text-[13px]">
+            Build modern, high-performance web applications with React and
+            Next.js, including server-side rendering and API integrations.
+          </p>
+          <div className="courseActions flex items-center justify-between mt-4 w-full">
+            <div className="rating flex items-center gap-1 text-md text-yellow-500">
+              <CiStar />
+              <CiStar />
+              <CiStar />
+              <CiStar />
+              <CiStar />
+              <span className="ratingValue text-black font-semibold">
+                5.0<span className="text-gray-500 font-normal">(1200)</span>
+              </span>
+            </div>
+            <div className="price font-semibold text-[#4A90E2]">
+              <h5>#1,500</h5>
+            </div>
+          </div>
+          <Link
+            href="/course/react-next-js"
+            className="w-full flex justify-center mt-5"
+          >
+            <button className="enrollButton px-4 py-2 bg-[#4A90E2] hover:bg-[#899cb1] hover:text-white rounded-md transition w-full">
+              Enroll Now
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomepageCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CourseBar from "./Component/CourseBar";
+import HomepageCard from "./Component/homepageCard";
 import TypingHero from "./Component/Typinghero";
-import Link from "next/link";
+// import Link from "next/link";
 
 export default async function Home() {
   // const response = await fetch("https://dummyjson.com/posts");
@@ -23,6 +24,24 @@ export default async function Home() {
 
       {/* The course main category bar starts here */}
       <CourseBar />
+      {/* The course main category bar ends here */}
+
+      {/* The homepage card section starts here */}
+      <section className="homepage-card-section w-[95%] md:w-[90%] mx-auto mt-3">
+        <h2 className="title text-center text-2xl md:text-4xl font-bold mb-6">
+          Explore Our Top Courses
+        </h2>
+        <div className="dummy flex flex-nowrap md:flex-wrap justify-center gap-4 overflow-x-scroll scroll-smooth">
+          <HomepageCard />
+          <HomepageCard />
+          <HomepageCard />
+          <HomepageCard />
+          <HomepageCard />
+          <HomepageCard />
+          <HomepageCard />
+          <HomepageCard />
+        </div>
+      </section>
     </>
   );
 }
